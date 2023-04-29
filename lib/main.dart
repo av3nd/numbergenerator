6cacdabbcc439e70feb9589783067b7d75d15fdf
Avend Tadbar <avendtabdar@gmail.com>
2023-04-29 15:58:09 +0545

    app still not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,40 +15,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        // useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Number Generator App'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +33,137 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _randomNumber1 = 0;
+  int _randomNumber2 = 0;
+  int _numPresses = 0;
 
-  void _incrementCounter() {
+  int _numGreaterPresses = 0;
+  int _numSmallerPresses = 0;
+  bool _isGameOver = false;
+
+  void _generateRandomNumbers() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _randomNumber1 = Random().nextInt(100);
+      _randomNumber2 = Random().nextInt(100);
+      _numPresses++;
+
+      if (_numPresses >= 10) {
+        _isGameOver = true;
+      }
+    });
+  }
+
+  void _onPressGreater() {
+    setState(() {
+      if (_randomNumber1 > _randomNumber2) {
+        _numGreaterPresses++;
+      } else if (_randomNumber2 > _randomNumber1) {
+        _numGreaterPresses++;
+      }
+      _generateRandomNumbers();
+    });
+  }
+
+  void _onPressSmaller() {
+    setState(() {
+      if (_randomNumber1 < _randomNumber2) {
+        _numSmallerPresses++;
+      } else if (_randomNumber2 < _randomNumber1) {
+        _numSmallerPresses++;
+      }
+      _generateRandomNumbers();
+    });
+  }
+
+  void _restartGame() {
+    setState(() {
+      _randomNumber1 = 0;
+      _randomNumber2 = 0;
+      _numPresses = 0;
+
+      _numGreaterPresses = 0;
+      _numSmallerPresses = 0;
+      _isGameOver = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      width: 150,
+                      height: 200,
+                      child: ElevatedButton(
+                          onPressed: !_isGameOver
+                              ? () => {
+                                    _generateRandomNumbers(),
+                                    _onPressGreater(),
+                                    _onPressSmaller()
+                                  }
+                              : null,
+                          child: Text('$_randomNumber1'))),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Container(
+                      width: 150,
+                      height: 200,
+                      child: ElevatedButton(
+                          onPressed: !_isGameOver
+                              ? () => {
+                                    _onPressGreater(),
+                                    _generateRandomNumbers(),
+                                    _onPressSmaller()
+                                  }
+                              : null,
+                          child: Text('$_randomNumber2'))),
+                ],
+              ),
+              SizedBox(height: 20),
+              _isGameOver
+                  ? Column(
+                      children: <Widget>[
+                        Text(
+                          'Correct Answer: $_numGreaterPresses',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                            color: Colors.green,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          'Incorrect Answer: $_numSmallerPresses',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                            color: Colors.red,
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: _restartGame,
+                          child: Text('Restart Game'),
+                        ),
+                      ],
+                    )
+                  : Container(),
+            ],
+          ),
+        ));
   }
 }
